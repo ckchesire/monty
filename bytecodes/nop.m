7ef90950e28acec62cf069106eec183b40245145
ckchesire <christianck.chesire@gmail.com>
2024-09-28 18:01:16 +0300

    Update3 the implemented nop opcode.

---
+++ bytecodes/nop.m
@@ -0,0 +1,6 @@
+nop
+push 0 Push 0 onto the stack
+push 1 Push 1 onto the stack
+ur ugly mama
+   nop your mama                        
+push 4
--- instruction2.c
+++ instruction2.c
@@ -99,6 +99,4 @@ void nop(stack_t **stack, unsigned int line_number)
 {
 	(void) stack;
 	(void) line_number;
-	free_all();
-	exit(0);
 }
